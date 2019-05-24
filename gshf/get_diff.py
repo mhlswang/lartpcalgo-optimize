5b4394b7056c7b0417b90c7f3125abbdd052844c
Brian J Gravelle <gravelle@cs.uoregon.edu>
2019-05-24 16:08:43 -0700

    better differencing

--- gshf/get_diff.py
+++ gshf/get_diff.py
@@ -1,4 +1,25 @@
+'''
+ok so
+fprintf(fout,"%d %d %d %lf %lf %lf %lf %lf\n",
+od_vec[iv][ic_min].n,od_vec[iv][ic_min].imh,od_vec[iv][ic_min].ipp,
+rd_vec[iv].simtck,rd_vec[iv].rectck,rd_vec[iv].rms,
+od_vec[iv][ic_min].mytck,od_vec[iv][ic_min].mysigma)
 
+
+rd_vec[iv].simtck is the simulated time of the hit
+rd_vec[iv].rectck is the reconstructed time of the hit using larsoft
+rd_vec[iv].rms is the spread of the hit using larsoft (not sure if it's simulated or reconstructed - but I can check)
+
+od_vec[iv][ic_min].mytck is the  reconstructed time of the hit using  our fitter
+od_vec[iv][ic_min].mysigma is the reconstructed spread of the gaussian using our fitter
+
+
+so we can compare `rd_vec[iv].rms` to `od_vec[iv][ic_min].mysigma`
+and `rd_vec[iv].simtck` compared to `rd_vec[iv].rectck` tells you how good larsoft is at finding the time
+which then we can compare to our versions of the fitterr
+by looking at `rd_vec[iv].rms` compared to `od_vec[iv][ic_min].mytck` for both marqfit and MKL
+
+'''
 
 
 
@@ -18,8 +39,12 @@ if __name__ == '__main__':
 
 	for old,new in zip(known_li,new_li):
 		out = ""
+		i = 0
 		for o,n in zip(old,new):
-			out = out + str(o-n) + ','
+			if i > 2:
+				out = out + str((o-n)/o) + ','
+			i = i + 1 
+
 		print out
 
 	f_new.close()
