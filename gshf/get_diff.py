089bebf9f67b19329e1d2ee8fa646423bbd37aea
Brian J Gravelle <gravelle@cs.uoregon.edu>
2019-05-30 16:19:23 -0700

    histogram and other plots

--- gshf/get_diff.py
+++ gshf/get_diff.py
@@ -80,29 +80,73 @@ if __name__ == '__main__':
 	new = ResultData("mkl_results.txt")
 
 
-	old_rms   = old.get_column("rd_vec.rms")
+	# basic comparison of the results from our implementations
+	old_sigma = old.get_column("od_vec.mysigma")
 	old_mytck = old.get_column("od_vec.mytck")
-	new_rms   = new.get_column("rd_vec.rms")
+	new_sigma = new.get_column("od_vec.mysigma")
 	new_mytck = new.get_column("od_vec.mytck")
- 	diff_rms   = [o-n for o,n in zip(old_rms,new_rms)]
+ 	diff_sigma = [o-n for o,n in zip(old_sigma,new_sigma)]
  	diff_mytck = [o-n for o,n in zip(old_mytck,new_mytck)]
 
-	x = range(len(old_rms))
+	x = range(len(old_sigma))
 
-	plt.figure(1)
-	plt.plot(x, new_rms, 'bs',  ms=0.7)
-	plt.plot(x, old_rms, 'r^', ms=0.2)
-	plt.savefig('pos0.png')
+	plt.figure()
+	plt.plot(x, new_sigma, 'bs',  ms=0.7)
+	plt.plot(x, old_sigma, 'r^', ms=0.2)
+	plt.savefig('lart_sigma.png')
 
-	plt.figure(2)
+	plt.figure()
 	plt.plot(x, new_mytck, 'bs',  ms=0.7)
 	plt.plot(x, old_mytck, 'r^', ms=0.2)
-	plt.savefig('pos1.png')
+	plt.savefig('lart_mytck.png')
  
-	plt.figure(3)
+	plt.figure()
 	plt.plot(x, diff_mytck, 'bs',  ms=0.7)
-	plt.plot(x, diff_rms,   'r^', ms=0.2)
-	plt.savefig('pos2.png')
+	plt.savefig('lart_diff_mytck.png')
+
+	plt.figure()
+	counts, bins = np.histogram(diff_mytck, bins=len(old_sigma)/10)
+	plt.hist(bins[:-1], bins, weights=counts, log=True)
+	plt.savefig('lart_hist_mytck.png')
+
+	plt.figure()
+	plt.plot(x, diff_sigma,   'r^', ms=0.2)
+	plt.savefig('lart_diff_sigma.png')
+
+
+
+	# comparison to LArSoft
+	old_simtck = old.get_column("rd_vec.simtck")
+	old_rectck = old.get_column("rd_vec.rectck")
+	new_simtck = new.get_column("rd_vec.simtck")
+	new_rectck = new.get_column("rd_vec.rectck")
+
+	# rd_vec[iv].simtck-rd_vec[iv].rectck=resolution of the time of 
+    # the fitter in larsoft compared to the truth
+	old_larsoft_accuracy = [s-r for s,r in zip(old_simtck,old_rectck)]
+	new_larsoft_accuracy = [s-r for s,r in zip(new_simtck,new_rectck)]
+
+	plt.figure()
+	plt.plot(x, new_larsoft_accuracy,   'bs', ms=0.2)
+	plt.savefig('lart_new_lart_accuracy.png')
+
+	plt.figure()
+	plt.plot(x, old_larsoft_accuracy,   'r^', ms=0.2)
+	plt.savefig('lart_old_lart_accuracy.png')
+
+	# rd_vec[iv].simtck-od_vec[iv][ic_min].mytck = resolution of the
+	# marqfit and MKL versions compared to truth
+	marquat_accuracy  = [s-r for s,r in zip(old_simtck,old_mytck)]
+	mkl_accuracy      = [s-r for s,r in zip(new_simtck,new_mytck)]
+
+	plt.figure()
+	plt.plot(x, new_larsoft_accuracy,   'bs', ms=0.2)
+	plt.savefig('lart_maquat_accuracy.png')
+
+	plt.figure()
+	plt.plot(x, old_larsoft_accuracy,   'r^', ms=0.2)
+	plt.savefig('lart_mkl_accuracy.png')
+
 
 
 
